10e97e11764d4b1a94c3635e4bcf3b908cf75840
Paul Hemmings <paulhemmings@gmail.com>
2016-02-09 16:25:56 -0800

    configure Eureka

--- src/main/java/com/razor/booter/EurekaServer.java
+++ src/main/java/com/razor/booter/EurekaServer.java
@@ -8,6 +8,13 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * Created by paul.hemmings on 2/9/16.
  */
 
+/**
+ * https://ahus1.github.io/hystrix-examples/manual.html#_ensuring_a_clean_start_up_and_shut_down_of_hystrix
+ * http://cloud.spring.io/spring-cloud-netflix/
+ * http://jobcoding.net/feed/view?feedIdx=37505
+ * http://localhost:8761/
+ */
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServer {
